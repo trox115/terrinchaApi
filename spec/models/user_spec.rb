ec7e8dbf42a7f55ed12f4cf659f5cfa94df892fd
trox115 <tofernandes67@gmail.com>
2020-05-28 23:44:01 +0100

    scope test for user done

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context 'Validation tests' do 
+    it 'ensures email presence' do
+    end
+
+    it 'ensures name presence' do
+    end
+    it 'should save successfuly' do
+    end
+  end
+
+  context 'scope Tests' do
+  end
 end
