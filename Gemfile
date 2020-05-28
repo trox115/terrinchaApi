f62348af1e6075cf17ae96b26d459be3de6723f3
trox115 <tofernandes67@gmail.com>
2020-05-29 00:29:15 +0100

    Model produtos validation tests

--- Gemfile
+++ Gemfile
@@ -46,3 +46,4 @@ end
 gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
 gem 'rspec-rails'
 gem 'rubocop'
+gem 'simplecov', require: false, group: :test
--- spec/models/cliente_spec.rb
+++ spec/models/cliente_spec.rb
@@ -1,5 +1,31 @@
 require 'rails_helper'
 
 RSpec.describe Cliente, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+ context 'Validation tests' do
+   it 'ensures name presence'do
+     cliente = Cliente.new(email:'António').save
+     expect(cliente).to eq(false)
+   end
+
+   it 'ensures email presence' do
+     cliente = Cliente.new(name:'António').save
+     expect(cliente).to eq(false)
+   end
+
+   it 'ensures phone presence' do
+     cliente = Cliente.new(name:'António').save
+     expect(cliente).to eq(false)
+   end
+
+   it 'ensures cliente can not be saved' do
+     cliente = Cliente.new(name:'António', phone:'1234', email:'algo@alog.com').save
+     expect(cliente).to eq(false)
+   end
+
+   it 'ensures cliente can not be saved' do
+     cliente = Cliente.new(name:'António', phone:'123456789', email:'algo@alog.com').save
+     expect(cliente).to eq(true)
+   end
+
+ end
 end
