658eef4502288a9beabb194ce70b059d9f8345e9
trox115 <tofernandes67@gmail.com>
2020-05-29 12:47:23 +0100

    new fix for linter

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -13,11 +13,13 @@ RSpec.describe User, type: :model do
     end
 
     it 'should not save successfuly' do
-      user = User.new(name: 'Antonio', email: 'tofernandes67@gmail.com', password: '1234', password_confirmation: '1234').save
+      user = User.new(name: 'Antonio', email: 'email@gmail.com',
+                      password: '1234', password_confirmation: '1234').save
       expect(user).to eq(false)
     end
     it 'should save successfuly' do
-      user = User.new(name: 'Antonio', email: 'tofernandes67@gmail.com', password: '123456789', password_confirmation: '123456789').save
+      user = User.new(name: 'Antonio', email: 'email@gmail.com',
+                      password: '123456789', password_confirmation: '123456789').save
       expect(user).to eq(true)
     end
   end
