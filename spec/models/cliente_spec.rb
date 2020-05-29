bcbe44843cd3a1f42d8ffe7f9eee45b330474c59
trox115 <tofernandes67@gmail.com>
2020-05-29 12:34:12 +0100

    Fix linter issues

--- spec/models/cliente_spec.rb
+++ spec/models/cliente_spec.rb
@@ -1,31 +1,30 @@
 require 'rails_helper'
 
 RSpec.describe Cliente, type: :model do
- context 'Validation tests' do
-   it 'ensures name presence'do
-     cliente = Cliente.new(email:'António').save
-     expect(cliente).to eq(false)
-   end
+  context 'Validation tests' do
+    it 'ensures name presence' do
+      cliente = Cliente.new(email: 'António').save
+      expect(cliente).to eq(false)
+    end
 
-   it 'ensures email presence' do
-     cliente = Cliente.new(name:'António').save
-     expect(cliente).to eq(false)
-   end
+    it 'ensures email presence' do
+      cliente = Cliente.new(name: 'António').save
+      expect(cliente).to eq(false)
+    end
 
-   it 'ensures phone presence' do
-     cliente = Cliente.new(name:'António').save
-     expect(cliente).to eq(false)
-   end
+    it 'ensures phone presence' do
+      cliente = Cliente.new(name: 'António').save
+      expect(cliente).to eq(false)
+    end
 
-   it 'ensures cliente can not be saved' do
-     cliente = Cliente.new(name:'António', phone:'1234', email:'algo@alog.com').save
-     expect(cliente).to eq(false)
-   end
+    it 'ensures cliente can not be saved' do
+      cliente = Cliente.new(name: 'António', phone: '1234', email: 'algo@alog.com').save
+      expect(cliente).to eq(false)
+    end
 
-   it 'ensures cliente can not be saved' do
-     cliente = Cliente.new(name:'António', phone:'123456789', email:'algo@alog.com').save
-     expect(cliente).to eq(true)
-   end
-
- end
+    it 'ensures cliente can not be saved' do
+      cliente = Cliente.new(name: 'António', phone: '123456789', email: 'algo@alog.com').save
+      expect(cliente).to eq(true)
+    end
+  end
 end
