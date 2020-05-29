329a3cceff2313c304a051646854a8113131de27
trox115 <tofernandes67@gmail.com>
2020-05-29 21:59:54 +0100

    Products displaying on right page

---
+++ app/controllers/produtos_controller.rb
@@ -0,0 +1,6 @@
+class ProdutosController < ApplicationController
+  def index
+    @produtos=Produto.all
+    render json: @produtos
+  end
+end
---
+++ spec/requests/produtos_request_spec.rb
@@ -0,0 +1,12 @@
+require 'rails_helper'
+
+RSpec.describe "Produtos", type: :request do
+
+  describe "GET /index" do
+    it "returns http success" do
+      get "/produtos/index"
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+end
