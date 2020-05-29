0fdc6308faaaa4e0a1fc4320d00dba077e93cb08
trox115 <tofernandes67@gmail.com>
2020-05-29 12:30:00 +0100

    test validates user models

--- app/models/produto.rb
+++ app/models/produto.rb
@@ -1,2 +1,6 @@
 class Produto < ApplicationRecord
+  validates :nome, presence:true
+  validates :descricao,presence:true, length:{maximum:15}
+  validates :preco, presence:true
+  validates :ano, presence:true
 end
--- spec/models/produto_spec.rb
+++ spec/models/produto_spec.rb
@@ -1,5 +1,33 @@
 require 'rails_helper'
 
 RSpec.describe Produto, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context 'validation tests' do
+    it 'ensures nome presence' do
+      produto = Produto.new(descricao:'teste', ano:'2003', preco:'5').save
+      expect(produto).to eq(false)
+    end
+
+    it 'ensures descricao presence' do
+      produto = Produto.new(nome:'teste', ano:'2003', preco:'5').save
+      expect(produto).to eq(false)
+    end
+
+    it 'ensures ano presence' do
+      produto = Produto.new(nome:'teste',descricao:'2003', preco:'5').save
+      expect(produto).to eq(false)
+    end
+
+    it 'ensures price presence' do
+      produto = Produto.new(nome:'teste',descricao:'2003',ano:'5').save
+      expect(produto).to eq(false)
+    end
+    it 'ensures product its not save successfuly presence' do
+      produto = Produto.new(nome:'teste',descricao:'2003 and here will have too much chars', preco:'5',ano:'5').save
+      expect(produto).to eq(false)
+    end
+    it 'ensures product its save successfuly presence' do
+      produto = Produto.new(nome:'teste',descricao:'2003 ', preco:'5',ano:'5').save
+      expect(produto).to eq(true)
+    end
+  end
 end
