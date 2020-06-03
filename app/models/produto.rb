69f49de36abf8a3b0cee9fc0902ee882700cfa00
trox115 <tofernandes67@gmail.com>
2020-06-03 22:46:28 +0100

    products on db fix length description

--- app/models/produto.rb
+++ app/models/produto.rb
@@ -1,6 +1,6 @@
 class Produto < ApplicationRecord
   validates :nome, presence: true
-  validates :descricao, presence: true, length: { maximum: 15 }
+  validates :descricao, presence: true, length: { maximum: 25 }
   validates :preco, presence: true
   validates :ano, presence: true
 end
