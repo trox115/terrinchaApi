b4918b3d55f00f3aa15db5d57ab9bbf92018f770
trox115 <tofernandes67@gmail.com>
2020-06-05 23:21:50 +0100

    generate tests for encomenda

--- app/models/encomenda.rb
+++ app/models/encomenda.rb
@@ -1,4 +1,5 @@
 class Encomenda < ApplicationRecord
   belongs_to :cliente
   belongs_to :produto
+  validates :quantidade, presence: true
 end
--- spec/models/encomenda_spec.rb
+++ spec/models/encomenda_spec.rb
@@ -1,5 +1,26 @@
 require 'rails_helper'
 
 RSpec.describe Encomenda, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context 'validation tests' do
+    it 'ensures that cliente id is present' do
+      cliente = Cliente.new(name:'Antonio',email:'antonio@email.com',phone:'123456789').save
+      encomenda = Encomenda.new(produto_id:1,quantidade:5 ).save
+      expect(encomenda).to eq(false)
+    end
+    it 'ensures that produto_id is present' do
+      cliente = Cliente.new(name:'Antonio',email:'antonio@email.com',phone:'123456789').save
+      encomenda = Encomenda.new(cliente_id:1,quantidade:5 ).save
+      expect(encomenda).to eq(false)
+    end
+    it 'ensures that quantidade is present' do
+      cliente = Cliente.new(name:'Antonio',email:'antonio@email.com',phone:'123456789').save
+      encomenda = Encomenda.new(cliente_id:1,produto_id:1).save
+      expect(encomenda).to eq(false)
+    end
+    it 'ensures that encomenda can be saved' do
+      cliente = Cliente.new(name:'Antonio',email:'antonio@email.com',phone:'123456789').save
+      encomenda = Encomenda.new(cliente_id:1,produto_id:1, quantidade:5).save
+      expect(encomenda).to eq(true)
+    end
+  end
 end
