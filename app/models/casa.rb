5ba6afd1dd2d6d449aeadf470e727138beef721c
trox115 <tofernandes67@gmail.com>
2020-06-07 03:00:29 +0100

    start work with casas

---
+++ app/models/casa.rb
@@ -0,0 +1,2 @@
+class Casa < ApplicationRecord
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_05_215117) do
+ActiveRecord::Schema.define(version: 2020_06_07_013309) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "casas", force: :cascade do |t|
+    t.string "nome"
+    t.boolean "limpa", default: true
+    t.boolean "ocupada", default: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "clientes", force: :cascade do |t|
     t.string "name"
