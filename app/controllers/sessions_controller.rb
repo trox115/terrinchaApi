099304741bc4232aa8a32b5b02c548e2eb745018
trox115 <tofernandes67@gmail.com>
2020-06-08 16:52:38 +0100

    fix linter issues

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -14,7 +14,7 @@ class SessionsController < ApplicationController
     else
       render json: params[user].errors, status: :unprocessable_entity
     end
-    end
+  end
 
   def logged_in
     if @current_user
