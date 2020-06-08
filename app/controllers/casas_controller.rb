82e8994a690ec211ed1ab300b7440432d8e319a6
trox115 <tofernandes67@gmail.com>
2020-06-08 12:01:22 +0100

    update casa

--- app/controllers/casas_controller.rb
+++ app/controllers/casas_controller.rb
@@ -3,4 +3,8 @@ class CasasController < ApplicationController
     @casas = Casa.all
     render json: @casas
   end
+  def update
+    casa = Casa.find(params[:id])
+    casa.update(ocupada: true)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   delete :logout, to: "sessions#logout"
+  put 'casas/:id', to: 'casas#update'
   post 'sessions/create'
   get 'sessions/logged_in'
     match '*all', controller: 'application', action: 'cors_preflight_check', via: [:options]
