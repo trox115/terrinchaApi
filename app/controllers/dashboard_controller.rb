cd8f95e531c25fd6e938fbb2bfb83c3a5e3a974a
trox115 <tofernandes67@gmail.com>
2020-08-04 09:39:00 +0100

    new changes

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,5 +1,10 @@
 class DashboardController < ApplicationController
   before_action :authenticate_administrador!
   def index
+    @users = User.all
+    @casa = Casa.all
+    @ocupada = Casa.where(ocupada:true)
+    @livres = Casa.where(limpa:true,ocupada:false)
+    @encomendas = Encomenda.all
   end
 end
